799bd5924c1c60cc86a4ffeca923a278772dc763
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-03 13:36:04 -0300

    temp: create Teacher List

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './assets/styles/global.css'
 
-import Landing from './pages/Landing'
+import Routes from './Routes'
+
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
