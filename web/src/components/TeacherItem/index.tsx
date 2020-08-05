b2244216693830b0c86bd073fb09a2ad17b4e0fb
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-05 14:00:33 -0300

    day 3

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,35 +4,56 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+export interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections',{
+      user_id: teacher.id,
+    })
+  }
   return (
     <article className="teacher-item">
 
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/45442467?s=460&u=dab2b54b701c4d4b7d1b40904bf27fd79908f4c9&v=4" alt="Perfil Name" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Idyl Icaro</strong>
-          <span>Matematica</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta em matematica computacional.
-          <br />
-          Expressoes numericas são legais, mas o melhor é logica matematica que é uma delicia.
-          </p>
+        {teacher.bio}
+      </p>
 
       <footer>
         <p>
           Preço/hora
-            <strong>R$ 100,00</strong>
+            <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a 
+        target="_blank"
+        onClick={createNewConnection} 
+        href={`https://wa.me/${teacher.whatsapp}`} 
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
               Entrar em contato
-            </button>
+            </a>
       </footer>
     </article>
   );
