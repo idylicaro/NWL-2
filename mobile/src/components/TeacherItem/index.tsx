93e40d3bba0f593898c8eb68d51df40313be057d
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-08-08 00:07:23 -0300

    feat: use APIs

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,47 +1,97 @@
-import React from 'react'
-import { View, Image, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Image, Text, Linking } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles'
+import api from '../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+
+interface TeacherItemProps{
+  teacher: Teacher
+  favorited: boolean
+}
+
+const TeacherItem:React.FC<TeacherItemProps> = ({ teacher, favorited }) =>{
+  const [isFavorited, setIsFavorited] = useState(favorited)
+  
+  function handleLinkToWhatsapp() {
+    api.post('/connections', {
+      user_id: teacher.id,
+    })
+
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+  }
+
+  async function handleToggleFavorite() {
+    const favorites = await AsyncStorage.getItem('favorites')
+
+    let favoritesArray = []
+      
+    if (favorites){
+      favoritesArray = JSON.parse(favorites)
+    }
+
+    if(isFavorited){
+      const favoritedIndex = favoritesArray.findIndex((teacherItem: Teacher) => teacherItem === teacher.id)
+      favoritesArray.splice(favoritedIndex,1)
+    }else{
+      favoritesArray.push(teacher)
+
+    }
+      setIsFavorited(!isFavorited)
+      await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray))
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <Image
           style={styles.avatar}
-          source={{ uri: 'https://github.com/idylicaro.png' }}
+          source={{ uri: teacher.avatar }}
         />
 
         <View style={styles.profileInfo}>
-          <Text style={styles.name}>Idyl Icaro</Text>
-          <Text style={styles.subject}>Matématica</Text>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
         </View>
 
       </View>
 
-      <Text style={styles.bio}>
-        BLA BLA BLA BLA BLA BLA BLA
-      </Text>
+      <Text style={styles.bio}>{teacher.bio}</Text>
 
       <View style={styles.footer}>
         <Text style={styles.price}>
           Preço/hora {'  '}
-          <Text style={styles.priceValue}>$ 20,00</Text>
+          <Text style={styles.priceValue}>$ {teacher.cost}</Text>
         </Text>
 
         <View style={styles.buttonsContainer}>
-          <RectButton style={[styles.favoriteButton, styles.favorited]}>
-            {/*<Image source={heartOutlineIcon} />*/}
-            <Image source={unfavoriteIcon} />
+          <RectButton 
+            onPress={handleToggleFavorite}
+            style={[
+              styles.favoriteButton,
+              isFavorited ? styles.favorited : {} 
+              ]}
+            
+          >
+            { isFavorited ? <Image source={unfavoriteIcon} /> : <Image source={heartOutlineIcon} />}
           </RectButton>
           
-          <RectButton style={styles.contactButton}>
+          <RectButton style={styles.contactButton} onPress={handleLinkToWhatsapp}>
             <Image source={whatsappIcon} />
             <Text style={styles.contactButtonText}>Entrar em contato</Text>
           </RectButton>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,20 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import TeacherItem, { Teacher } from '../../components/TeacherItem'
 
 import { Feather } from '@expo/vector-icons'
 
+import api from '../../services/api'
+
 import styles from './styles'
+import { useFocusEffect } from '@react-navigation/native'
 
 function TeacherList() {
   const [isFiltersVisible, setIsFilterVisible] = useState(false);
+  const [teachers, setTeachers] = useState([])
+  const [favorites, setFavorites] = useState<number[]>([])
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response =>{
+      if(response) {
+        const favoritedTeachers = JSON.parse(response)
+        const favoritedTeachersIds = favoritedTeachers.map((teacher:Teacher) => teacher.id)
+        setFavorites(favoritedTeachersIds)
+      }
+    })
+  }
 
   function handlerToggleFiltersVisible(){
     setIsFilterVisible(!isFiltersVisible)
   }
+
+  async function handleFiltersSubmit(){
+    loadFavorites()
+    const response = await api.get('/classes',{
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    })
+    setIsFilterVisible(false)
+    setTeachers(response.data)
+  }
+
+  useFocusEffect(()=>{
+    loadFavorites()
+  })
 
   return (
     <View style={styles.container}>
@@ -31,6 +67,8 @@ function TeacherList() {
             <Text style={styles.label}>Matéria</Text>
             <TextInput
               style={styles.input}
+              value={subject}
+              onChangeText={text => setSubject(text)}
               placeholder="Qual a matéria?"
               placeholderTextColor="#c1bccc"
             />
@@ -40,6 +78,8 @@ function TeacherList() {
                 <Text style={styles.label}>Dia da semana</Text>
                 <TextInput
                   style={styles.input}
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
                   placeholder="Qual o dia?"
                   placeholderTextColor="#c1bccc"
                 />
@@ -49,13 +89,15 @@ function TeacherList() {
                 <Text style={styles.label}>Horário</Text>
                 <TextInput
                   style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
                   placeholder="Qual horário?"
                   placeholderTextColor="#c1bccc"
                 />
               </View>
             </View>
 
-            <RectButton style={styles.submitButton}>
+            <RectButton style={styles.submitButton} onPress={handleFiltersSubmit}>
               <Text style={styles.submitButtonText}>Filtrar</Text>
             </RectButton>
           </View>)
@@ -69,10 +111,14 @@ function TeacherList() {
           paddingBottom: 16
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) =>{
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />)
+        })}
       </ScrollView>
     </View>
   )
